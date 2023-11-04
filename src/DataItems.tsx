77eee0f64938a40c87add6a3abc623ab205a2fbd
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 15:53:20 +0600

    Доболнил меню

--- src/DataItems.tsx
+++ src/DataItems.tsx
@@ -1,7 +1,13 @@
-export const INGREDIENTS = [
-    { name: 'Salad', price: 10},
-    { name: 'Cheese', price: 50},
-    { name: 'Meat', price: 80},
-    { name: 'Bacon', price: 60},
+export const MENU = [
+    { name: 'Sandwich', price: 200},
+    { name: 'Hamburger', price: 180},
+    { name: 'Pizza', price: 350},
+    { name: 'Bacon', price: 80},
     {name: 'Coffe', price: 100},
+    {name: 'Tea', price: 50},
+    {name: 'Cake', price: 70},
+    {name: 'Chiken Soup', price: 150},
+    {name: 'Cola', price: 60},
+    {name: 'Juice', price: 80},
+    {name: 'Fries', price: 50},
 ];
