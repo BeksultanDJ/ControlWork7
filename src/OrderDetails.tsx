69b2eef7f235ca7e29bdc321cd970e5f20b8f425
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 18:16:03 +0600

    Добавил класс для заголовка Добавил проверку на пустоту заказа

--- src/OrderDetails.tsx
+++ src/OrderDetails.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Dishes from './AddItems';
-import { MENU } from './DataItems';
+import {MENU} from './DataItems';
 
 function Order() {
     const [selectedDishes, setSelectedDishes] = useState<{ [key: string]: number }>({});
@@ -21,7 +21,7 @@ function Order() {
     };
 
     const removeDish = (dishName: string) => {
-        const updatedDishes = { ...selectedDishes };
+        const updatedDishes = {...selectedDishes};
         if (updatedDishes[dishName] && updatedDishes[dishName] > 0) {
             updatedDishes[dishName] -= 1;
             setSelectedDishes(updatedDishes);
@@ -33,25 +33,34 @@ function Order() {
     return (
         <div className="Order">
             <div className="OrderDetails">
-                <h3>Order Details:</h3>
-                {Object.keys(selectedDishes).map((dishName) => {
-                    const dishCount = selectedDishes[dishName];
-                    if (dishCount > 0) {
-                        const menuItem = MENU.find((item) => item.name === dishName);
-                        const dishPrice = menuItem ? menuItem.price : 0;
-                        return (
-                            <div key={dishName} className={`${dishName} OrderedDish`}>
-                                {`${dishName} x${dishCount}`}
-                                <p>Price: {`${dishPrice} som`}</p>
-                                <button onClick={() => removeDish(dishName)}>Remove</button>
-                            </div>
-                        );
-                    }
-                    return null;
-                })}
-                <p className="Price">Total price: {orderPrice} som</p>
+                <h3 className="OrderDetailsTitle">Order Details:</h3>
+                {Object.keys(selectedDishes).length > 0 ? (
+                    Object.keys(selectedDishes).map((dishName) => {
+                        const dishCount = selectedDishes[dishName];
+                        if (dishCount > 0) {
+                            const menuItem = MENU.find((item) => item.name === dishName);
+                            const dishPrice = menuItem ? menuItem.price : 0;
+                            return (
+                                <div key={dishName} className={`${dishName} OrderedDish`}>
+                                    {`${dishName} x${dishCount}`}
+                                    <p>Price: {`${dishPrice} som`}</p>
+                                    <button onClick={() => removeDish(dishName)}>Remove</button>
+                                </div>
+                            );
+                        }
+                        return null;
+                    })
+                ) : (
+                    <div className="Notice">
+                        <p>Your order is empty.</p>
+                        <p>Please add some dishes.</p>
+                    </div>
+                )}
+                {Object.keys(selectedDishes).length > 0 && (
+                    <p className="Price">Total price: {orderPrice} som</p>
+                )}
             </div>
-            <Dishes onDishChange={handleDishChange} selectedDishes={selectedDishes} />
+            <Dishes onDishChange={handleDishChange} selectedDishes={selectedDishes}/>
         </div>
     );
 }
