76565829812bb68423defc1d784fe3b6ced94f6b
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-04 17:40:36 +0600

    Переопределил класс для кнопки

--- src/AddItems.tsx
+++ src/AddItems.tsx
@@ -1,36 +1,39 @@
-import { MENU } from './DataItems';
+import {MENU} from './DataItems';
 
 interface MenuDishProps {
     onDishChange: (selectedDishes: { [key: string]: number }) => void;
     selectedDishes: { [key: string]: number };
 }
 
-function MenuItems({ onDishChange, selectedDishes }: MenuDishProps) {
+function MenuItems({onDishChange, selectedDishes}: MenuDishProps) {
     const increaseQuantity = (dishName: string) => {
-        const updatedDishes = { ...selectedDishes };
+        const updatedDishes = {...selectedDishes};
         updatedDishes[dishName] = (selectedDishes[dishName] || 0) + 1;
         onDishChange(updatedDishes);
     };
 
     const decreaseQuantity = (dishName: string) => {
         if (selectedDishes[dishName] && selectedDishes[dishName] > 0) {
-            const updatedDishes = { ...selectedDishes };
+            const updatedDishes = {...selectedDishes};
             updatedDishes[dishName] -= 1;
             onDishChange(updatedDishes);
         }
     };
 
     return (
-        <div className="CreatingOrderBlock">
-            {MENU.map((dish) => (
-                <div key={dish.name} className="MenuItems">
-                    <button onClick={() => increaseQuantity(dish.name)}>
-                        <span>{dish.name}</span>
-                        <p>Price: {dish.price}</p>
-                    </button>
-                    <button onClick={() => decreaseQuantity(dish.name)}>Remove</button>
-                </div>
-            ))}
+        <div>
+            <h3 className="MenuTitle">Menu</h3>
+            <div className="CreatingOrderBlock">
+                {MENU.map((dish) => (
+                    <div key={dish.name}>
+                        <button className="MenuItems" onClick={() => increaseQuantity(dish.name)}>
+                            <span>{dish.name}</span>
+                            <p>Price: {dish.price}</p>
+                        </button>
+                        <button onClick={() => decreaseQuantity(dish.name)}>Remove</button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
